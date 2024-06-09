819a1226b0524a816a78a03b977f212a43d17f0a
hemakattumenu <hemakattumenu@gmail.com>
2024-06-09 13:45:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1= Math.floor((Math.random()*6)+1);
+var randomDice1 ="./images/dice"+randomNumber1+".png";
+
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+var randomNumber2=Math.floor((Math.random()*6)+1);
+var randomDice2 ="./images/dice"+randomNumber2+".png";
+
+
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩player1 wins!";
+}else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="player2 wins!🚩";
+}else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
